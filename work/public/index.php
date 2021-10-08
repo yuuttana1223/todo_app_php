1fef08b24bac76fd6cadb1072f9e7eb4605c99b8
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-08 20:58:16 +0900

    [Add] データ(todos)の取得を追加

--- work/public/index.php
+++ work/public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 const DSN = "mysql:host=db;dbname=myapp;charset=utf8mb4";
 const DB_USER = "myappuser";
 const DB_PASS = "myapppass";
@@ -10,12 +13,23 @@ try {
     DB_PASS,
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES => false,
     ]
   );
 } catch (PDOException $e) {
   echo $e->getMessage();
   exit;
 }
+
+function getTodos(PDO $pdo): array
+{
+  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+  $todos = $stmt->fetchAll();
+  return $todos;
+}
+
+$todos = getTodos($pdo);
 ?>
 
 <!DOCTYPE html>
@@ -24,10 +38,25 @@ try {
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
   <h1>Todos</h1>
+  <ul>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+    <li>
+      <input type="checkbox" checked>
+      <span class="done">Title</span>
+    </li>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+  </ul>
 </body>
 
 </html>
