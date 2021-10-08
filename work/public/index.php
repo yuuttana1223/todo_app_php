c1406038558bd92af6dd4a7bae6dd6a3a218230b
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-08 20:21:53 +0900

    [Add] PDOの設定を追加

--- work/public/index.php
+++ work/public/index.php
@@ -1,10 +1,33 @@
+<?php
+const DSN = "mysql:host=db;dbname=myapp;charset=utf8mb4";
+const DB_USER = "myappuser";
+const DB_PASS = "myapppass";
+
+try {
+  $pdo = new PDO(
+    DSN,
+    DB_USER,
+    DB_PASS,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]
+  );
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
 </head>
+
 <body>
   <h1>Todos</h1>
 </body>
+
 </html>
