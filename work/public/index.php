0789e958e8918690d6dcfc37d295c75dd148b804
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-08 21:20:34 +0900

    [Add] データをHTMLに埋め込む

--- work/public/index.php
+++ work/public/index.php
@@ -22,6 +22,11 @@ try {
   exit;
 }
 
+function h(string $str): string
+{
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+
 function getTodos(PDO $pdo): array
 {
   $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
@@ -44,18 +49,12 @@ $todos = getTodos($pdo);
 <body>
   <h1>Todos</h1>
   <ul>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
-    <li>
-      <input type="checkbox" checked>
-      <span class="done">Title</span>
-    </li>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
+    <?php foreach ($todos as $todo) : ?>
+      <li>
+        <input type="checkbox" <?= $todo->is_done ? "checked" : ""; ?>>
+        <span class=<?= $todo->is_done ? "done" : ""; ?>><?= h($todo->title); ?></span>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </body>
 
