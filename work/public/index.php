6e7d78884afcf77cbcf4ffdcbe956043fe4a2b6d
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-08 22:02:23 +0900

    [Add] CSRF対策でtokenを作成

--- work/public/index.php
+++ work/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+session_start();
 
 const DSN = "mysql:host=db;dbname=myapp;charset=utf8mb4";
 const DB_USER = "myappuser";
@@ -8,6 +9,8 @@ const DB_PASS = "myapppass";
 // const SITE_URL = "http://localhost:8562";
 // constだと変数は使えないからエラーが出る
 define("SITE_URL", "http://{$_SERVER["HTTP_HOST"]}");
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -30,6 +33,20 @@ function h(string $str): string
   return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
+function createToken(): void
+{
+  if (!isset($_SESSION["token"])) {
+    $_SESSION["token"] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken(): void
+{
+  if (empty($_SESSION["token"]) || $_SESSION["token"] !== filter_input(INPUT_POST, "token")) {
+    exit("Invalid post request");
+  }
+}
+
 function addTodo(PDO $pdo): void
 {
   $title = trim(filter_input(INPUT_POST, "title"));
@@ -50,6 +67,7 @@ function getTodos(PDO $pdo): array
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  validateToken();
   addTodo($pdo);
 
   header("Location: {SITE_URL}");
@@ -73,6 +91,7 @@ $todos = getTodos($pdo);
   <h1>Todos</h1>
   <form action="" method="post">
     <input type="text" name="title" placeholder="Type new todo. ">
+    <input type="hidden" name="token" value="<?= h($_SESSION["token"]) ?>">
   </form>
   <ul>
     <?php foreach ($todos as $todo) : ?>
